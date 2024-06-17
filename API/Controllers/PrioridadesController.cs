e96978e71e8ebc3ce481a232c3d9eace95dcc3f3
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-06-17 00:08:25 -0400

    Prioridad Entity , controller DbContexts for Mongo and Sql

---
+++ API/Controllers/PrioridadesController.cs
@@ -0,0 +1,60 @@
+﻿using API.Application.Commands.PrioridadesCommands;
+using API.Application.Dtos;
+using API.Application.Queries.PrioridadesQueries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PrioridadesController(IMediator _mediator) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<ActionResult<PrioridadesDto>> GetById(int id)
+    {
+        var priority = await _mediator.Send(new GetPrioridadByIdQuery(id));
+        if (priority == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(priority);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PrioridadesDto>>> GetAll()
+    {
+        return Ok(await _mediator.Send(new GetAllPrioridadQuery()));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> Create([FromBody] CrearPrioridadCommand command)
+    {
+        var prioridad = await _mediator.Send(command);
+        return CreatedAtAction(nameof(GetById), new { id = prioridad.Id }, prioridad);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, [FromBody] ActualizarPrioridadCommand command)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest();
+        }
+
+        await _mediator.Send(command);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var result = await _mediator.Send(new BorrarPrioridadCommand(id));
+        if (!result)
+        {
+            return NotFound();
+        }
+        return NoContent();
+    }
+}
---
+++ API/Domain/Entities/Prioridades.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Domain.Entities;
+
+public class Prioridades
+{
+    [Key]
+    public int Id { get; set; }
+    public string? Descripcion { get; set; }
+    public int DiasCompromiso { get; set; }
+}
