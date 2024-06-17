95024f03119dca7efd60de7a7df69f4697cd76ef
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-06-17 00:08:51 -0400

    DbContext Here

---
+++ API/Infratructure/Data/MongoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Domain.Entities;
+using MongoDB.Driver;
+
+namespace API.Infratructure.Data;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+    public MongoDbContext(string connectionString, string databaseName)
+    {
+        var client = new MongoClient(connectionString);
+        _database = client.GetDatabase(databaseName);
+    }
+
+    public IMongoCollection<Prioridades> Priorities => _database.GetCollection<Prioridades>("Prioridades");
+}
---
+++ API/Infratructure/Data/SqlDbContext.cs
@@ -0,0 +1,12 @@
+﻿using API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Infratructure.Data;
+
+public class SqlDbContext : DbContext
+{
+    public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options){}
+
+    public DbSet<Prioridades> Prioridades { get; set; }
+}
+
