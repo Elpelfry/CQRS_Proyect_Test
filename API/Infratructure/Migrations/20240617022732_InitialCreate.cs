b5b4c4ce966d3af5d9a303663635efc5cfc82f8f
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-06-17 00:09:19 -0400

    Migrations and MongoInitializer

---
+++ API/Infratructure/Migrations/20240617022732_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prioridades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DiasCompromiso = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prioridades", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prioridades");
+        }
+    }
+}
---
+++ API/Infratructure/Services/MongoDbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using API.Application.Dtos;
+using API.Domain.Entities;
+using MongoDB.Driver;
+
+namespace API.Infratructure.Services;
+
+public class MongoDbInitializer
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbInitializer(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetConnectionString("MongoConnection"));
+        _database = client.GetDatabase(configuration["ConnectionStrings:MongoDatabaseName"]);
+    }
+
+    public async Task InitializeAsync()
+    {
+        // Crear colecciones y definir índices si es necesario
+        var collectionNames = await _database.ListCollectionNames().ToListAsync();
+        if (!collectionNames.Contains("Prioridades"))
+        {
+            await _database.CreateCollectionAsync("Prioridades");
+            var collection = _database.GetCollection<Prioridades>("Prioridades");
+
+            // Crear índices u otras configuraciones necesarias
+            var indexKeysDefinition = Builders<Prioridades>.IndexKeys.Ascending(p => p.Id);
+            var indexOptions = new CreateIndexOptions { Unique = true };
+            var indexModel = new CreateIndexModel<Prioridades>(indexKeysDefinition, indexOptions);
+            await collection.Indexes.CreateOneAsync(indexModel);
+        }
+    }
+}
